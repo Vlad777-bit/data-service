8965c1d8c4736e3b347bf6593231fc6981a057db
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-11-07 16:23:03 +0300

    update controller

--- src/main/java/ru/nozhkin/data_service/controller/StudentController.java
+++ src/main/java/ru/nozhkin/data_service/controller/StudentController.java
@@ -1,4 +1,37 @@
 package ru.nozhkin.data_service.controller;
 
-public class StudentController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.nozhkin.data_service.api.StudentDataApi;
+import ru.nozhkin.data_service.data.Student;
+import ru.nozhkin.data_service.data.StudentRepository;
+import ru.nozhkin.data_service.model.StudentDataCreateRequest;
+import ru.nozhkin.data_service.model.StudentDataResponse;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
